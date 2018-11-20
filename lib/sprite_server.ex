d790cb6e25199da4c1db5a34b707ae3bedf236e8
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-19 21:28:57 -0700

    Add constrains on Sprite moves.

--- lib/sprite_server.ex
+++ lib/sprite_server.ex
@@ -1,41 +1,41 @@
 defmodule SpriteServer do
   use GenServer
 
-  def init(%sprite{} = initial_state), do: {:ok, initial_state}
+  def init(%Sprite{} = initial_state), do: {:ok, initial_state}
 
-  def handle_cast({:move_left, quantity}, sprite) do
+  def handle_cast({:move_left, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_left(sprite, quantity)}
   end
 
-  def handle_cast({:move_right, quantity}, sprite) do
+  def handle_cast({:move_right, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_right(sprite, quantity)}
   end
 
-  def handle_cast({:move_up, quantity}, sprite) do
+  def handle_cast({:move_up, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_up(sprite, quantity)}
   end
 
-  def handle_cast({:move_down, quantity}, sprite) do
+  def handle_cast({:move_down, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_down(sprite, quantity)}
   end
 
-  def handle_cast({:move_upper_left, quantity}, sprite) do
+  def handle_cast({:move_upper_left, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_upper_left(sprite, quantity)}
   end
 
-  def handle_cast({:move_upper_right, quantity}, sprite) do
+  def handle_cast({:move_upper_right, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_upper_right(sprite, quantity)}
   end
 
-  def handle_cast({:move_lower_left, quantity}, sprite) do
+  def handle_cast({:move_lower_left, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_lower_left(sprite, quantity)}
   end
 
-  def handle_cast({:move_lower_right, quantity}, sprite) do
+  def handle_cast({:move_lower_right, quantity}, %Sprite{} = sprite) do
     {:noreply, sprite.move_lower_right(sprite, quantity)}
   end
 
-  def handle_call(:status, _from, sprite) do
+  def handle_call(:status, _from, %Sprite{} = sprite) do
     {:reply, sprite, sprite}
   end
 end
