7ac0d311e56012caa42dabbb331aea39f530ae1d
Ruben Amortegui <ruben.amortegui@gmail.com>
2018-11-19 18:16:25 -0700

    Add movements for sprite.

--- lib/sprint.ex
+++ lib/sprint.ex
@@ -1,18 +1,48 @@
 defmodule Sprint do
+  defstruct [posx: 0, posy: 0, lives: 5]
+
   @moduledoc """
   Documentation for Sprint.
   """
 
-  @doc """
-  Hello world.
+  defguardp is_alive?(lives) when is_integer(lives) and lives > 0
 
-  ## Examples
-
-      iex> Sprint.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def move_right(%Sprint{posx: posx, lives: lives} = sprint, quantity \\ 1) when is_alive?(lives) do
+    %Sprint{ sprint | posx: posx + quantity }
   end
+
+  def move_left(%Sprint{posx: posx} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posx: posx - quantity }
+  end
+
+  def move_up(%Sprint{posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posy: posy + quantity }
+  end
+
+  def move_down(%Sprint{posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posy: posy - quantity }
+  end
+
+  def move_upper_left(%Sprint{posx: posx, posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posx: posx - quantity, posy: posy + quantity }
+  end
+
+  def move_upper_right(%Sprint{posx: posx, posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posx: posx + quantity, posy: posy + quantity }
+  end
+
+  def move_lower_left(%Sprint{posx: posx, posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posx: posx - quantity, posy: posy - quantity }
+  end
+
+  def move_lower_right(%Sprint{posx: posx, posy: posy} = sprint, quantity \\ 1) do
+    %Sprint{ sprint | posx: posx + quantity, posy: posy - quantity }
+  end
+
+  def decrease_lives(%Sprint{lives: lives} = sprint, quantity) do
+    %{sprint | lives: lives - quantity}
+  end
+
+  def alive?(%Sprint{lives: lives}) when is_alive?(lives), do: true
+  def alive?(%Sprint{}), do: false
 end
